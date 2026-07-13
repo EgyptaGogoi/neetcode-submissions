ee856362bbe58a2b55ea6d9eafac3e3b3f7e0fe0
Egypta Gogoi <egyptagogoi@gmail.com>
2026-07-13 15:19:47 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> freqT = new HashMap<>();
+        HashMap<Character, Integer> freqS = new HashMap<>();
+        for (int i=0; i< t.length(); i++){
+            freqT.put(t.charAt(i), freqT.getOrDefault(t.charAt(i), 0)+1);
+        }
+        for (int i=0; i< s.length(); i++){
+            freqS.put(s.charAt(i), freqS.getOrDefault(s.charAt(i), 0)+1);
+        }
+        if (t.length()>s.length()){
+            for (Character key : freqT.keySet()) {
+                if (freqS.get(key) != freqT.get(key))
+                    return false;
+            }
+        }
+        else{
+            for (Character key : freqS.keySet()) {
+                if (freqS.get(key) != freqT.get(key))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
