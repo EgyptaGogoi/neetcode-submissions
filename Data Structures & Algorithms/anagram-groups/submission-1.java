3f3d8dd2dfeee78aa2dee9276c8535eec8defb6a
Egypta Gogoi <egyptagogoi@gmail.com>
2026-07-21 12:50:53 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String sortString(String s){
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashSet<String> hs = new HashSet<>();
+        List<List<String>> ans = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+        for(String ele : strs){
+            String sorted_ele = sortString(ele);
+            hs.add(sorted_ele);
+        }
+        for(String unq : hs){
+            for(String ele : strs){
+                String sorted_ele = sortString(ele);
+                if(unq.equals(sorted_ele))
+                    temp.add(ele);
+            }
+             ans.add(new ArrayList<>(temp));
+            temp.clear();
+        }
+        return ans;
+    }
+}
