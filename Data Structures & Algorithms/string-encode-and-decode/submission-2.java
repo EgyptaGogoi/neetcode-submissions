5db6ac2bcdbcabfc818e9641e82ba875ea78fa15
Egypta Gogoi <egyptagogoi@gmail.com>
2026-07-25 23:32:26 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        String ans = new String("");
+        for(String ele: strs){
+            char[] chr = ele.toCharArray();
+            ans += (char)chr.length;
+            ans += '#';
+            ans += ele;
+        }
+        return ans;
+    }
+
+    public List<String> decode(String str) {
+        List<String> ans = new ArrayList();
+        int l = 0;
+        while(l<str.length()){
+            int len = str.charAt(l);
+            l+=2;
+            ans.add(str.substring(l,l+len));
+            l+=len;
+        }
+        return ans;
+    }
+}
