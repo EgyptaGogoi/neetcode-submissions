25583c5b0e88a6be840ff29e3349930997b1d5c9
Egypta Gogoi <egyptagogoi@gmail.com>
2026-09-14 13:13:51 +0530

    Time: 13 ms (31.30%) | Memory: 46.3 MB (25.81%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,13 +2,14 @@ class Solution {
     public boolean isPalindrome(String s) {
         if (s.length() == 1)
             return true;
-        s = s.toLowerCase();
-        s = s.replaceAll("[^a-zA-Z0-9]","");
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
         int i = 0;
-        int j = s.length()-1;
+        int j = s.length() - 1;
         while(i<j){
             if(s.charAt(i) != s.charAt(j))
                 return false;
+            // if(arr[i] != arr[j])
             i++;
             j--;
         }
