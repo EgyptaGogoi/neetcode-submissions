28a452f63cea472673ac08b6a9def581f2ca8f4a
Egypta Gogoi <egyptagogoi@gmail.com>
2026-09-19 22:52:17 +0530

    Time: 1 ms (99.95%) | Memory: 94.5 MB (33.02%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -6,8 +6,6 @@ class Solution {
         for(int i = 0; i< prices.length; i++){
             int curr_profit = 0;
             if(prices[i]<buy_p){
-                // if (sell_p > 0)
-                //     sell_p = 0;
                 buy_p = prices[i];
                 sell_p = 0;
             }
@@ -17,8 +15,6 @@ class Solution {
             if (curr_profit > max_profit)
                 max_profit = curr_profit;
         }
-        // System.out.println("buy_p"+ buy_p);
-        // System.out.println("sell_p"+ sell_p);
         return max_profit;
     }
 }
