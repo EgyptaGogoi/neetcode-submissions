9718af09db40c7f726bbbe2494b222870ced6646
Egypta Gogoi <egyptagogoi@gmail.com>
2026-09-19 22:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (65.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,25 +1,3 @@
-// class Solution {
-//     public int trap(int[] block) {
-//         if (block.length <= 2)
-//             return 0;
-//         int i = 0;
-//         int currvol = 0;
-//         int totvol = 0;
-//         int l = block.length;
-//         while(i<l-1 && block[i] < block[i+1]){
-//             i++;
-//         }
-//         for(int j = i+1; j<l-1 ; j++){
-            
-//             currvol+=diff;
-//             if(block[i] <= block[j+1]){
-//                 i=j+1;
-//                 totvol+=currvol;
-//             }
-//         }
-//         return totvol;
-//     }
-// }
 class Solution {
     public int trap(int[] block) {
         if (block.length <= 2)
@@ -35,7 +13,6 @@ class Solution {
         int max_l = block[j];
 
         while(i<j){
-            // int curr_min = Math.min(max_r, max_l);
             if (max_r <= max_l){
                 if(block[i+1] <= max_r)
                     totvol += max_r-block[i+1];
