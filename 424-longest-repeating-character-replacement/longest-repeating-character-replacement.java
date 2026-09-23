c0d42e2c089e0eddeb8481e430a4871cfcb19ca1
Egypta Gogoi <egyptagogoi@gmail.com>
2026-09-23 23:21:43 +0530

    Time: 18 ms (34.10%) | Memory: 46.2 MB (64.91%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getMaxFreq(int[] arr){
+        int max = 0;
+        for(int i: arr){
+            if (max < i)
+                max = i;
+        }
+        return max;
+    }
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int i = 0;
+        int j = 0;
+        int maxlen = 0;
+        while(j< s.length()){
+            freq[s.charAt(j)-'A']++;
+            while(j-i+1 - getMaxFreq(freq) > k){
+                freq[s.charAt(i)-'A']--;
+                i++;
+            }
+            maxlen = Math.max((j-i+1), maxlen);
+            j++;
+        }
+        return maxlen;
+    }
+}
