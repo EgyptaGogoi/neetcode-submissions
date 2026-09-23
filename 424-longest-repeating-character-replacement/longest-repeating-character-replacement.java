d9cb1b15f99111fd743c8f0a9d6a1d562652fc4a
Egypta Gogoi <egyptagogoi@gmail.com>
2026-09-23 23:40:11 +0530

    Time: 2 ms (99.92%) | Memory: 46.4 MB (30.97%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -13,7 +13,7 @@ class Solution {
         while(j< s.length()){
             freq[s.charAt(j)-'A']++;
             maxfreq = Math.max(maxfreq, freq[s.charAt(j)-'A']);
-            while(j-i+1 - maxfreq > k){
+            if(j-i+1 - maxfreq > k){
                 freq[s.charAt(i)-'A']--;
                 i++;
             }
